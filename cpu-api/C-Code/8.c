4c1bac8bc759e2a96b093d5645aae4f5c3576e4b
Squirlex <jakobzim20002@gmail.com>
2024-10-06 16:39:15 +0200

    Homework Chapter 5

--- cpu-api/C-Code/8.c
+++ cpu-api/C-Code/8.c
@@ -8,7 +8,11 @@ int
 main(int argc, char *argv[])
 {
     int pipearray[2]; // pipearray[0] input [1] ist output
-    pipe(pipearray);
+    if (pipe(pipearray) == -1){
+        perror("pipe");
+        exit(1);
+    };
+    char buffer[600];
 
     int rc = fork();
     if (rc < 0) {
@@ -33,8 +37,12 @@ main(int argc, char *argv[])
         } else if (re == 0) {
             // child2 (new process)
             close(pipearray[1]);
-            dup2(pipearray[0], STDIN_FILENO);
+            read(pipearray[0], buffer, sizeof(buffer));
+            //dup2(pipearray[0], STDIN_FILENO);
             close(pipearray[0]);
+            printf("hello from child2 \n");
+            printf("I'll tell you what child1 has to say: %s\n", buffer);
+            /**
             char * pointer = malloc(sizeof(char)*600); // 600 weil bestimmt groß genug
             char * pointer2 = malloc(sizeof(char)*600);
             char * pointer3 = malloc(sizeof(char)*600);
@@ -44,6 +52,7 @@ main(int argc, char *argv[])
             free(pointer);
             free(pointer2);
             free(pointer3);
+            **/
         } else {
             // parent goes down this path (original process)
             int w = waitpid(re, NULL, 0);
